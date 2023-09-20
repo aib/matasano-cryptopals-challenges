ca4a9cd5bf1782bf0034b5d8e8a987be80bc723f
aib <aibok42@gmail.com>
2023-09-20 14:43:45 +0300

    Set 3 Challenge 19

--- src/main.rs
+++ src/main.rs
@@ -890,4 +890,78 @@ fn main() {
 		println!("Set 3 Challenge 18: {}", bytes_to_string(&dec));
 		assert_eq!("0e15ad04b165a34e8fe2dea7f9dda0a128b03c86d913a1dc8fb34272e436a2bc", sha256str(&dec));
 	}
+
+	{ // Set 3 Challenge 19
+		let strings = [
+			"SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==",
+			"Q29taW5nIHdpdGggdml2aWQgZmFjZXM=",
+			"RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==",
+			"RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=",
+			"SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk",
+			"T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+			"T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=",
+			"UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+			"QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=",
+			"T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl",
+			"VG8gcGxlYXNlIGEgY29tcGFuaW9u",
+			"QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==",
+			"QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=",
+			"QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==",
+			"QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=",
+			"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+			"VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==",
+			"SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==",
+			"SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==",
+			"VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==",
+			"V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==",
+			"V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==",
+			"U2hlIHJvZGUgdG8gaGFycmllcnM/",
+			"VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=",
+			"QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=",
+			"VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=",
+			"V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=",
+			"SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==",
+			"U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==",
+			"U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=",
+			"VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==",
+			"QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu",
+			"SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=",
+			"VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs",
+			"WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=",
+			"SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0",
+			"SW4gdGhlIGNhc3VhbCBjb21lZHk7",
+			"SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=",
+			"VHJhbnNmb3JtZWQgdXR0ZXJseTo=",
+			"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+		].iter().map(|s| bytes_from_base64(&s)).collect::<Vec<_>>();
+
+		let key = get_random_bytes(16);
+		let ciphertexts = strings.iter()
+			.map(|pt| ctr_encrypt(aes_128_encrypt_block, 16, &key, &vec![0; 8], pt))
+			.collect::<Vec<_>>();
+
+		let transposed = (0..)
+			.map(|n| {
+				ciphertexts.iter().filter_map(|c| c.get(n).copied()).collect()
+			})
+			.take_while(|t: &Vec<u8>| t.len() > 0)
+			.collect::<Vec<_>>();
+
+		let mut probable_key_bytes = Vec::new();
+		for t in transposed {
+			let mut scored = (0..=255).map(|k| {
+				let dec = xor_encode(&t, &[k]);
+				let score = score_text(&bytes_to_string(&dec));
+				(k, dec, score)
+			}).collect::<Vec<_>>();
+			scored.sort_by(|kts1, kts2| kts1.2.total_cmp(&kts2.2));
+			probable_key_bytes.push(scored.pop().unwrap().0);
+		}
+
+		let probable_strings: Vec<String> = ciphertexts.iter()
+			.map(|ct| bytes_to_safe_string(&xor_encode(&ct, &probable_key_bytes)))
+			.collect();
+
+		println!("Set 3 Challenge 19: {} / {} / {} / {}", probable_strings[0], probable_strings[1], probable_strings[2], probable_strings[3]);
+	}
 }
